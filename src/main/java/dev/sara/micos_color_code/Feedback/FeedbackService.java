07dee9228bd9a165c98be5bc4b59452e1596fe80
Sara Vázquez <saravazlo@gmail.com>
2025-10-06 09:44:14 +0200

    feat: started service structure

--- src/main/java/dev/sara/micos_color_code/Feedback/FeedbackService.java
+++ src/main/java/dev/sara/micos_color_code/Feedback/FeedbackService.java
@@ -1,5 +1,10 @@
 package dev.sara.micos_color_code.Feedback;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedbackService {
     
+    private final JavaMailSender mailSender;
+
 }
