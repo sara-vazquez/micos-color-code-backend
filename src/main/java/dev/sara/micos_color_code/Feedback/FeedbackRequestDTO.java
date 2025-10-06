ef452a2bc3f6fb1110a5e635513e91557527dc35
Sara Vázquez <saravazlo@gmail.com>
2025-10-06 14:36:56 +0200

    fix: fixed and simplify feedback service

--- src/main/java/dev/sara/micos_color_code/Feedback/FeedbackRequestDTO.java
+++ src/main/java/dev/sara/micos_color_code/Feedback/FeedbackRequestDTO.java
@@ -3,4 +3,17 @@ package dev.sara.micos_color_code.Feedback;
 public class FeedbackRequestDTO {
     private String email;
     private String message;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
 }
--- src/main/java/dev/sara/micos_color_code/Feedback/FeedbackService.java
+++ src/main/java/dev/sara/micos_color_code/Feedback/FeedbackService.java
@@ -1,11 +1,21 @@
 package dev.sara.micos_color_code.Feedback;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FeedbackService {
     
-    private final JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender mailSender;
 
+    public void sendFeedback(FeedbackRequestDTO feedback) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("micoscolorcode@gmail.com");
+        message.setSubject("Nuevo feedback de Micos");
+        message.setText("📧 De: " + feedback.getEmail() + "\n\n📝 Mensaje:\n" + feedback.getMessage());
+        mailSender.send(message);
+    }
 }
