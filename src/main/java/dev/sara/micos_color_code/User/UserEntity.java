6a2fbab046592446068a0390f694c045e07edaf4
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 15:11:29 +0200

    fix: updated user entity table

--- src/main/java/dev/sara/micos_color_code/User/UserEntity.java
+++ src/main/java/dev/sara/micos_color_code/User/UserEntity.java
@@ -1,10 +1,18 @@
 package dev.sara.micos_color_code.User;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import dev.sara.micos_color_code.Role.RoleEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,5 +40,9 @@ public class UserEntity {
 
     @Column(nullable = false)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> roles = new HashSet<>(); 
     
 }
