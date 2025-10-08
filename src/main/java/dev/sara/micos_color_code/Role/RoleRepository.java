5df91a01d194a2af2d82ecb921f99e0fbc2a608d
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 15:30:47 +0200

    fix: fixed role repository change string value

--- src/main/java/dev/sara/micos_color_code/Role/RoleRepository.java
+++ src/main/java/dev/sara/micos_color_code/Role/RoleRepository.java
@@ -5,5 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-    Optional<RoleEntity> findByUsername(String username);
+    Optional<RoleEntity> findByName(String name);
 }
