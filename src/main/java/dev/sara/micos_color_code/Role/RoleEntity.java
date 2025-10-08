a73778908eab367e27d71daf88a973a9cbf3d8d4
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 14:45:49 +0200

    feat: created Role repository

--- src/main/java/dev/sara/micos_color_code/Role/RoleEntity.java
+++ src/main/java/dev/sara/micos_color_code/Role/RoleEntity.java
@@ -2,8 +2,19 @@ package dev.sara.micos_color_code.Role;
 
 import java.util.Set;
 
-import jakarta.persistence.*;
-import lombok.*;
+import dev.sara.micos_color_code.User.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = "roles")
@@ -21,6 +32,6 @@ public class RoleEntity {
     private String username;
 
     @ManyToMany(mappedBy="roles")
-    private Set<UserEntity> users
+    private Set<UserEntity> users;
 
 }
---
+++ src/main/java/dev/sara/micos_color_code/Role/RoleRepository.java
@@ -0,0 +1,9 @@
+package dev.sara.micos_color_code.Role;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByUsername(String username);
+}
