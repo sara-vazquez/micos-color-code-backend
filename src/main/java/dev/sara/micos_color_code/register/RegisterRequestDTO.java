2efe2fbeee21851f44d3c5f4ae35f31f97978c22
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 19:24:29 +0200

    feat: created register request dto

---
+++ src/main/java/dev/sara/micos_color_code/register/RegisterRequestDTO.java
@@ -0,0 +1,12 @@
+package dev.sara.micos_color_code.register;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDTO {
+    private String username;
+    private String email;
+    private String password;
+}
