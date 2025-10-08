99a34e6a2fe2c176d22390f61e978febf617ce8e
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 15:42:43 +0200

    fix: updated userentity with enabled column for security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO roles (id, name) VALUES (1, "ROLE_ADMIN");
-INSERT INTO roles (id, name) VALUES (2, "ROLE_USER");
+INSERT INTO roles (id_role, name) VALUES (1, "ROLE_ADMIN");
+INSERT INTO roles (id_role, name) VALUES (2, "ROLE_USER");
 
 /* Test: admin and user */
 
