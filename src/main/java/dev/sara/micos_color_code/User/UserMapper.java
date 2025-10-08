8b8956732e7b306980cda61961e255c5f1b7574c
Sara Vázquez <saravazlo@gmail.com>
2025-10-08 09:14:48 +0200

    fix: fixed user mapper

--- src/main/java/dev/sara/micos_color_code/User/UserMapper.java
+++ src/main/java/dev/sara/micos_color_code/User/UserMapper.java
@@ -1,16 +1,22 @@
 package dev.sara.micos_color_code.User;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
-    
-    public static UserResponseDTO toResponseDTO(UserEntity entity) {
-        if (entity == null) {
-            return null;
-        }
-        
-        return new UserResponseDTO(
-            entity.getId(),
-            entity.getUsername(),
-            entity.getEmail()
-        );
+
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserEntity toEntity(UserRequestDTO dto) {
+        return UserEntity.builder()
+        .username(dto.username())
+        .email(dto.email())
+        .password(passwordEncoder.encode(dto.password()))
+        .build();
+    }
+
+    public UserResponseDTO toResponse(UserEntity entity) {
+        return new UserResponseDTO(entity.getId(), entity.getUsername(), entity.getEmail());
     }
 }
