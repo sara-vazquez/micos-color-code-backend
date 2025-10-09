4fedf4756218eaca7542823ac0cc3721d5054ca0
Sara Vázquez <saravazlo@gmail.com>
2025-10-09 09:06:38 +0200

    fix: updated security with auth endpoints

--- src/main/java/dev/sara/micos_color_code/config/SecurityConfig.java
+++ src/main/java/dev/sara/micos_color_code/config/SecurityConfig.java
@@ -7,9 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +25,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${jwt.key}")
@@ -24,6 +33,9 @@ public class SecurityConfig {
 
     @Autowired
     private CorsConfigurationSource corsConfigurationSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public JwtDecoder jwtDecoder() {
@@ -33,24 +45,54 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
+            .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(corsConfigurationSource))
             
             .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register", "/feedback").permitAll()
             .requestMatchers(HttpMethod.POST, "/feedback").permitAll()
             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+            // Admin endpoints
+            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+
+            // User endpoints - also admin can use
+            .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
                 
             // Any other request should be authenticated
             .anyRequest().authenticated()
             )
+
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             
             .oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwt -> jwt.decoder(jwtDecoder()))
-            );
+            )
+
+            .authenticationProvider(authenticationProvider);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
     
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,3 @@
 INSERT INTO roles (id_role, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO roles (id_role, name) VALUES (2, 'ROLE_USER');
 
--- TEST: USER AND ADMIN 
-INSERT INTO users (id, username, email, password, enabled)
-VALUES (1, 'admin_test', 'admin@test.com', 'admin12345678!', TRUE);
-
-INSERT INTO users (id, username, email, password, enabled)
-VALUES (2, 'user_test', 'user@test.com', 'user12345678!', TRUE);
-
--- REL.
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
-INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
