06f387781066eb97f81cfd5365cfad52f648800c
Sara Vázquez <saravazlo@gmail.com>
2025-10-11 15:27:06 +0200

    feat: created a message dto for confirmation

---
+++ src/main/java/dev/sara/micos_color_code/util/ApiMessageDTO.java
@@ -0,0 +1,10 @@
+package dev.sara.micos_color_code.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiMessageDTO {
+    private String message;
+}
