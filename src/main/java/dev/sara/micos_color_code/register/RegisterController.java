cc7555e4116138cd0d916e876ea0f75a69349607
Sara Vázquez <saravazlo@gmail.com>
2025-10-11 17:44:10 +0200

    fix: updated register controller with new method confirm

--- src/main/java/dev/sara/micos_color_code/register/RegisterController.java
+++ src/main/java/dev/sara/micos_color_code/register/RegisterController.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.sara.micos_color_code.Captcha.CaptchaService;
@@ -33,5 +35,15 @@ public class RegisterController {
 
         RegisterResponseDTO response = registerService.register(request);
     return ResponseEntity.ok(response);
-}
+    }
+
+    @GetMapping("/confirm")
+    public String confirmAccount(@RequestParam("token") String token) {
+        try {
+            registerService.confirmAccount(token);
+            return "redirect:http://localhost:5173/confirmation-success";
+        } catch (RuntimeException e) {
+            return "redirect:http://localhost:5173/confirmation-error?message=" + e.getMessage();
+        }
+    }
 }
