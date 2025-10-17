e4f8224077fbf452cc51894d7d059a1b5bad80fb
Sara Vázquez <saravazlo@gmail.com>
2025-10-17 15:29:55 +0200

    feat: created dtos

---
+++ src/main/java/dev/sara/micos_color_code/means/ResourceRequestDTO.java
@@ -0,0 +1,3 @@
+package dev.sara.micos_color_code.means;
+
+public record ResourceRequestDTO(String image, String name, String intro, String descrption, String pdf) {}
---
+++ src/main/java/dev/sara/micos_color_code/means/ResourceResponseDTO.java
@@ -0,0 +1,5 @@
+package dev.sara.micos_color_code.means;
+
+public record ResourceResponseDTO(Long id, String image, String name, String intro, String descrption, String pdf) {
+    
+}
