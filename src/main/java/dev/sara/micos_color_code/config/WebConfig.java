2a787c4389e522e1e1b9d88d2216607a3e32a480
Sara Vázquez <saravazlo@gmail.com>
2025-10-18 16:23:10 +0200

    feat: created web configuration class for making uploads folder public

---
+++ src/main/java/dev/sara/micos_color_code/config/WebConfig.java
@@ -0,0 +1,13 @@
+package dev.sara.micos_color_code.config;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+}
