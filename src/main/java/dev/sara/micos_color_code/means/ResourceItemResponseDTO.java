139fde035fdd5a545672928a0cc7cf9cbd4c7944
Sara Vázquez <saravazlo@gmail.com>
2025-10-18 16:01:02 +0200

    fix: fixed resource item dto

--- src/main/java/dev/sara/micos_color_code/means/ResourceItemResponseDTO.java
+++ src/main/java/dev/sara/micos_color_code/means/ResourceItemResponseDTO.java
@@ -1,5 +1,3 @@
 package dev.sara.micos_color_code.means;
 
-public record ResourceItemResponseDTO(Long id, String name, String intro) {
-    
-}
+public record ResourceItemResponseDTO(Long id, String name, String intro, String description, String imageFile, String pdfFile) {}
