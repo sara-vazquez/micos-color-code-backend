17a0b71eb25092ed8b41d0febe714d24feee6121
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 12:44:01 +0200

    fix: updated game entity with sessions lists

--- src/main/java/dev/sara/micos_color_code/play/GameEntity.java
+++ src/main/java/dev/sara/micos_color_code/play/GameEntity.java
@@ -1,10 +1,17 @@
 package dev.sara.micos_color_code.play;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import dev.sara.micos_color_code.sessions.GameSessionEntity;
+import dev.sara.micos_color_code.stats.UserGameStatsEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,5 +34,11 @@ public class GameEntity {
 
     @Column(name = "gameName", nullable = false, unique = true)
     private String gameName;
+
+    @OneToMany(mappedBy="game", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserGameStatsEntity> gameStats = new HashSet<>();
+
+    @OneToMany(mappedBy="game", cascade = CascadeType.ALL)
+    private Set<GameSessionEntity> sessions = new HashSet<>();
     
 }
