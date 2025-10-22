a87c1b0193a78c1a09298dead287586826d4ac20
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 21:01:07 +0200

    fix: fixed game sessions service

--- src/main/java/dev/sara/micos_color_code/sessions/GameSessionService.java
+++ src/main/java/dev/sara/micos_color_code/sessions/GameSessionService.java
@@ -3,9 +3,10 @@ package dev.sara.micos_color_code.sessions;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dev.sara.micos_color_code.play.GameEntity;
 import dev.sara.micos_color_code.play.GameNotFoundException;
 import dev.sara.micos_color_code.play.GameRepository;
-import dev.sara.micos_color_code.stats.UserGameStatsRepository;
+import dev.sara.micos_color_code.stats.UserGameStatsService;
 import dev.sara.micos_color_code.user.UserEntity;
 import dev.sara.micos_color_code.user.UserRepository;
 
@@ -14,34 +15,34 @@ public class GameSessionService {
 
     private final GameSessionRepository gameSessionRepository;
     private final GameRepository gameRepository;
-    private final UserGameStatsRepository userGameStatsRepository;
     private final UserRepository userRepository;
+    private final UserGameStatsService userGameStatsService;
 
-    public GameSessionService(GameSessionRepository gameSessionRepository, GameRepository gameRepository, UserGameStatsRepository userGameStatsRepository, UserRepository userRepository) {
+    public GameSessionService(GameSessionRepository gameSessionRepository, GameRepository gameRepository, UserGameStatsService userGameStatsService, UserRepository userRepository) {
         this.gameSessionRepository = gameSessionRepository;
         this.gameRepository = gameRepository;
-        this.userGameStatsRepository = userGameStatsRepository;
+        this.userGameStatsService = userGameStatsService;
         this.userRepository = userRepository;
     }
 
     @Transactional
-    public GameSessionResponseDTO completeGameSession(Long userId, Long gameId, GameSessionRequestDTO gameSessionRequestDTO) {
-        UserEntity user = userRepository.findById(userId);
+    public GameSessionResponseDTO completeGameSession(Long userId, Long gameId, GameSessionRequestDTO request) {
+        UserEntity user = userRepository.findById(userId)
             .orElseThrow(() -> new GameNotFoundException("Usuario no encontrado"));
 
-        GameEntity game = gameRepository.findById(gameId);
+        GameEntity game = gameRepository.findById(gameId)
             .orElseThrow(() -> new GameNotFoundException("Juego no encontrado"));
 
         GameSessionEntity session = GameSessionEntity.builder() //save session
-            .user(user);
-            .game(game);
+            .user(user)
+            .game(game)
             .points(request.points())
             .timeCompleted(request.timeCompleted())
             .build();
         gameSessionRepository.save(session);
 
         //updates Ranking
-        int newTotalPoints = userGameStatsService.updateStats(userId, gameId, request.points());
+        int newTotalPoints = userGameStatsService.updateUserStats(userId, gameId, request.points());
         return new GameSessionResponseDTO(request.points(), newTotalPoints);
     }
     
