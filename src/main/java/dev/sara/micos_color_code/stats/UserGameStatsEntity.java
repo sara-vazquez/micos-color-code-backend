6ff981a8627f097e6a58b1e29687cf55aa2b0c8c
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 10:44:12 +0200

    feat: created userGameStatsEntity

---
+++ src/main/java/dev/sara/micos_color_code/stats/UserGameStatsEntity.java
@@ -0,0 +1,43 @@
+package dev.sara.micos_color_code.stats;
+
+import dev.sara.micos_color_code.play.GameEntity;
+import dev.sara.micos_color_code.user.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="user_game_stats", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "game_id"})})
+public class UserGameStatsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id", nullable = false)
+    private GameEntity game;
+
+    @Column(name="total_points", nullable = false)
+    private int totalPoints;
+    
+}
