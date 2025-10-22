7a01019edaafaa22b37816d6b24fea065f1d2d7b
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 17:49:07 +0200

    feat: created user game statsService

--- src/main/java/dev/sara/micos_color_code/stats/UserGameStatsService.java
+++ src/main/java/dev/sara/micos_color_code/stats/UserGameStatsService.java
@@ -1,8 +1,87 @@
 package dev.sara.micos_color_code.stats;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
+
+import dev.sara.micos_color_code.play.GameEntity;
+import dev.sara.micos_color_code.play.GameNotFoundException;
+import dev.sara.micos_color_code.play.GameRepository;
+import dev.sara.micos_color_code.user.UserEntity;
+import dev.sara.micos_color_code.user.UserRepository;
 
 @Service
 public class UserGameStatsService {
-    
+    private final UserGameStatsRepository userGameStatsRepository;
+    private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+
+    public UserGameStatsService(UserGameStatsRepository userGameStatsRepository, UserRepository userRepository, GameRepository gameRepository) {
+        this.userGameStatsRepository = userGameStatsRepository;
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
+    }
+
+    @Transactional
+    public int updateUserStats(Long userId, Long gameId, int pointsToAdd) {
+        UserEntity user = userRepository.findById(userId)
+            .orElseThrow(() -> new GameNotFoundException("Usuario no encontrado"));
+        GameEntity game = gameRepository.findById(gameId)
+            .orElseThrow(() -> new GameNotFoundException("Juego no encontrado"));
+
+            UserGameStatsEntity stats = userGameStatsRepository
+            .findByUser_IdAndGame_Id(userId, gameId)
+            .orElse(UserGameStatsEntity.builder()
+                .user(user)
+                .game(game)
+                .totalPoints(0)
+                .build());
+
+        stats.setTotalPoints(stats.getTotalPoints() + pointsToAdd);
+        userGameStatsRepository.save(stats);
+        
+        return stats.getTotalPoints();
+    }
+
+    // TOP 3
+    public RankingResponseDTO getRanking(Long gameId, Long currentUserId) {
+        Pageable top3Pageable = PageRequest.of(0, 3);
+        List<UserGameStatsEntity> top3Stats = userGameStatsRepository
+            .findByGame_IdOrderByTotalPointsDesc(gameId, top3Pageable);
+        
+        List<RankingPlayerDTO> top3 = new ArrayList<>();
+        for (int i = 0; i < top3Stats.size(); i++) {
+            UserGameStatsEntity stat = top3Stats.get(i);
+            top3.add(new RankingPlayerDTO(
+                stat.getUser().getUsername(),
+                stat.getTotalPoints(),
+                i + 1
+            ));
+        }
+
+        //Current user
+        UserGameStatsEntity currentUserStats = userGameStatsRepository
+            .findByUser_IdAndGame_Id(currentUserId, gameId)
+            .orElse(null);
+        
+        RankingPlayerDTO currentUser = null;
+        if (currentUserStats != null) {
+            Long playersAhead = userGameStatsRepository
+                .countPlayersWithHigherScore(gameId, currentUserStats.getTotalPoints());
+            int position = playersAhead.intValue() + 1;
+            
+            currentUser = new RankingPlayerDTO(
+                currentUserStats.getUser().getUsername(),
+                currentUserStats.getTotalPoints(),
+                position
+            );
+        }
+        
+        return new RankingResponseDTO(top3, currentUser);
+    }
 }
