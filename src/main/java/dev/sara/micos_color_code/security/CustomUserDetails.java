2d66fa374bba345b1de02d2af394ea1c270d9af7
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 20:40:36 +0200

    feat: created custom user details class for implementing auth interface

---
+++ src/main/java/dev/sara/micos_color_code/security/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package dev.sara.micos_color_code.security;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import dev.sara.micos_color_code.user.UserEntity;
+
+public class CustomUserDetails implements UserDetails {
+    private final UserEntity user;
+
+    public CustomUserDetails(UserEntity user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+    
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+            .map(role -> new SimpleGrantedAuthority(role.getName()))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+}
