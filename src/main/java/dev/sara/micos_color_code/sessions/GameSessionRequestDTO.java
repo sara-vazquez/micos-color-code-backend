0119363878bea530fbfca3f929f0c14a7884e519
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 15:39:19 +0200

    feat: created game sessions dtos

---
+++ src/main/java/dev/sara/micos_color_code/sessions/GameSessionRequestDTO.java
@@ -0,0 +1,6 @@
+package dev.sara.micos_color_code.sessions;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record GameSessionRequestDTO(@NotNull(message = "Los puntos son obligatorios") @Min(value = 0, message = "Los puntos no pueden ser negativos")int points,int timeCompleted ) {}
---
+++ src/main/java/dev/sara/micos_color_code/sessions/GameSessionResponseDTO.java
@@ -0,0 +1,3 @@
+package dev.sara.micos_color_code.sessions;
+
+public record GameSessionResponseDTO(int sessionPoints, int newTotalPoints) {}
