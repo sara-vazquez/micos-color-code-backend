6a9af926cd8d6f881d0f1ffe49379d2716323743
Sara Vázquez <saravazlo@gmail.com>
2025-10-23 08:59:12 +0200

    feat: added game to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,5 @@
 INSERT IGNORE INTO roles (id_role, name) VALUES (1, 'ROLE_ADMIN');
 INSERT IGNORE INTO roles (id_role, name) VALUES (2, 'ROLE_USER');
 
+-- GAMES
+INSERT INTO games (id, name) VALUES (1, 'Memory Cards');
