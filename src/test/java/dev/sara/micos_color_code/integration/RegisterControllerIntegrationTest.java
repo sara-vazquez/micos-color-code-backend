a85067bdcf67dc91c81e7fd4b95bc0fba822ede5
Sara Vázquez <saravazlo@gmail.com>
2025-10-29 21:21:50 +0100

    test: integration test still not pass, maybe bc data.sql

--- src/test/java/dev/sara/micos_color_code/integration/RegisterControllerIntegrationTest.java
+++ src/test/java/dev/sara/micos_color_code/integration/RegisterControllerIntegrationTest.java
@@ -31,6 +31,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -47,6 +48,7 @@ import dev.sara.micos_color_code.util.EmailService;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class RegisterControllerIntegrationTest {
 
     @Autowired
