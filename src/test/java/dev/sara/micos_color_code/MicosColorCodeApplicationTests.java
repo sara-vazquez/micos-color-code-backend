0190f4b906af8202f8703e22d60921b0bd37740d
Sara Vázquez <saravazlo@gmail.com>
2025-10-30 11:33:53 +0100

    test: created application properties test

--- src/test/java/dev/sara/micos_color_code/MicosColorCodeApplicationTests.java
+++ src/test/java/dev/sara/micos_color_code/MicosColorCodeApplicationTests.java
@@ -2,8 +2,10 @@ package dev.sara.micos_color_code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("tests")
 class MicosColorCodeApplicationTests {
 
 	@Test
